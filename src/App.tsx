2f1808f00beea0d2c35b65973a7116336f6c6d3d
RedEmber-hub <redember38@gmail.com>
2026-05-07 21:01:23 +0300

    добавила библиотеку с частицами

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import ParticlesBackground from './components/effects/Particles/Particles';
 import { HomePage } from './components/organisms/HomePage';
 
 export default function App() {
   return (
     <>
       <div className="app">
+        <ParticlesBackground />
         <HomePage />
       </div>
     </>
