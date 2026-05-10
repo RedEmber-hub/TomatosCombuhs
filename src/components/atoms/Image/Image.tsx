43368d8b4406d5163364baf94d1062e5ae08667e
RedEmber-hub <redember38@gmail.com>
2026-05-10 17:54:35 +0300

    настроила стили и адаптив, добавила скелетон

--- src/components/atoms/Image/Image.tsx
+++ src/components/atoms/Image/Image.tsx
@@ -1,6 +1,26 @@
 import { ImageProps } from './Image.type';
 import './Image.scss';
+import { useState } from 'react';
 
 export default function Image({ src, alt }: ImageProps) {
-  return <img src={src} alt={alt} className="image" />;
+  const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState(false);
+
+  return (
+    <div className="image__wrapper">
+      {!loaded && !error && <div className="skeleton skeleton--animated" />}
+
+      {!error && (
+        <img
+          src={src}
+          alt={alt}
+          className={`image ${loaded ? 'image--visible' : ''}`}
+          onLoad={() => setLoaded(true)}
+          onError={() => setError(true)}
+        />
+      )}
+
+      {error && <div className="image__error">Image failed to load</div>}
+    </div>
+  );
 }
